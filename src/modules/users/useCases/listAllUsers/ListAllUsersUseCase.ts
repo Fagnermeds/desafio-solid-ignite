c0c57b04ac98269ca9c8bf2476d01ff5d15152f8
fagnermeds <fagnermeds@gmail.com>
2022-01-13 11:44:25 -0300

    feat: add features to solve challenge

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,14 +2,26 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id: any;
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    if (!user.admin) {
+      throw new Error("You are not an admin user");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
